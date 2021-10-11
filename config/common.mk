8fa157c794423ff1f44c964911bb6ef574d14334
Michael Bestas <mkbestas@lineageos.org>
2021-10-11 23:44:14 +0300

    config: Remove TWRP config * This hasn't been supported for years.

--- config/common.mk
+++ config/common.mk
@@ -88,11 +88,6 @@ ifneq ($(TARGET_DISABLE_LINEAGE_SDK), true)
 include vendor/heavens/config/heavens_sdk_common.mk
 endif
 
-# TWRP
-ifeq ($(WITH_TWRP),true)
-include vendor/heavens/config/twrp.mk
-endif
-
 # Do not include art debug targets
 PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
 
